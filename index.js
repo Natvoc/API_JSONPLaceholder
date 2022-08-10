6449a28c0fa130dd5463c2ae74f745809b60f0c1
Natyari Vargas <naryari@gmail.com>
2022-08-10 07:59:33 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const API_URL = 'https://jsonplaceholder.typicode.com/users';
 
 const xhr = XMLHttpRequest();
+
+function onRequestHandler(){
+    if(this.readyState === 4 && this.status === 200){
+        console.log(this.response);
+    }
+}
+
+xhr.addEventListener('load', onRequestHandler);
